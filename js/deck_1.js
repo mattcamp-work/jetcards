321be40d90ba2b21b3ffbecfa99f9918edd7c233
Matt Camp <matt@mattcamp.com>
2014-05-03 23:52:27 -0400

    First pass on layout
    First pass on layout
    
    Signed-off-by: Matt Camp <matt@mattcamp.com>

--- js/deck_1.js
+++ js/deck_1.js
@@ -1,10 +1,36 @@
-var deck = function();
+var Deck = function(){};
 
-var card = function();
+var Card = function(){};
 
-card._front = "";
-card._back = "";
-card._id = "";
+Card.prototype._front = "";
+Card.prototype._back = "";
+Card.prototype._id = "";
+
+
+
+//var Deck = new Array([]);
+
+Deck.prototype._name = "test deck";
+Deck.prototype._subject = "state capitals";
+Deck.prototype.cards = new Array([]);
+
+
+Deck.prototype.card_count = function(){
+	console.log(this.cards.length);
+};
+
+
+var matt = new Deck();
+
+matt._name = "matt's deck";
+matt._subject = "dogs";
+
+for (var i=0;i<20;i++)
+{ 
+matt.cards[i] = new Card();
+};
+
+
 
 
 
